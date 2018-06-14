6542ee93aeea89e258de6a8f3ef1c44211fc549a
alanpadillachua <axp141330@utdallas.edu>
2018-06-14 10:50:17 -0500

    made the receive more golike

--- goreceiver/goreceive.go
+++ goreceiver/goreceive.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"os/exec"
 )
 
@@ -12,9 +14,33 @@ func handleErr(err error) {
 }
 
 func main() {
-	udpsendcmd := exec.Command("udp-receiver", "--pipe", "\"gzip -d\"", "--file", "warpeace.txt")
+	udpcastcmd := "udp-receiver"
+	udpcastargs := []string{"--pipe", "\"gzip -d\"", "--file", "warpeace.txt"}
 
-	sendout, err := udpsendcmd.Output()
-	handleErr(err)
-	fmt.Println(string(sendout))
+	cmd := exec.Command(udpcastcmd, udpcastargs...)
+	cmdReader, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error creating StdoutPipe for Cmd", err)
+		os.Exit(1)
+	}
+
+	scanner := bufio.NewScanner(cmdReader)
+	go func() {
+		for scanner.Scan() {
+			fmt.Printf(" udpcast output | %s\n", scanner.Text())
+		}
+	}()
+
+	err = cmd.Start()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error starting Cmd", err)
+		os.Exit(1)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error waiting for Cmd", err)
+		os.Exit(1)
+	}
+
 }
