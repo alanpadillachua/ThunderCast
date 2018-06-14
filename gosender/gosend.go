64bee852628af11a2902a10b2740d28c190354ad
alanpadillachua <axp141330@utdallas.edu>
2018-06-14 14:01:32 -0500

    go send working

--- gosender/gosend.go
+++ gosender/gosend.go
@@ -1,7 +1,6 @@
-package main
+package gocastsend
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
@@ -13,36 +12,31 @@ func handleErr(err error) {
 	}
 }
 
-func main() {
+func send(file string) {
 	//udpsendcmd := exec.Command("udp-sender", "--async", "--file", "samplefiles/warpeace.txt")
 	//udpsendcmd := exec.Command("udp-sender", "--async", "--fec", "8x8", "--pipe", "\"gzip -fN\"", "--file", "samplefiles/warpeace.txt")
+
 	udpcastcmd := "udp-sender"
-	udpcastargs := []string{"--async", "--fec", "8x8", "--pipe", "\"gzip -fN\"", "--file", "samplefiles/warpeace.txt"}
-
+	//"--pipe", "\"gzip -f\"",
+	udpcastargs := []string{"--async", "--fec", "8x8", "--file", file}
 	cmd := exec.Command(udpcastcmd, udpcastargs...)
-	cmdReader, err := cmd.StdoutPipe()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error creating StdoutPipe for Cmd", err)
-		os.Exit(1)
-	}
-
-	scanner := bufio.NewScanner(cmdReader)
-	go func() {
-		for scanner.Scan() {
-			fmt.Printf(" udpcast output | %s\n", scanner.Text())
-		}
-	}()
-
-	err = cmd.Start()
+	out, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error starting Cmd", err)
 		os.Exit(1)
 	}
+	fmt.Printf("%s\n", out)
+	/*
+		err = cmd.Start()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error starting Cmd", err)
+			os.Exit(1)
+		}
 
-	err = cmd.Wait()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error waiting for Cmd", err)
-		os.Exit(1)
-	}
-
+		err = cmd.Wait()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error waiting for Cmd", err)
+			os.Exit(1)
+		}
+	*/
 }
