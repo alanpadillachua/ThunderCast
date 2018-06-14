424039f03a963179a91088caf3dbf2d6d45311e1
alanpadillachua <axp141330@utdallas.edu>
2018-06-14 10:19:17 -0500

    fixed exec to send

--- gosender/gosend.go
+++ gosender/gosend.go
@@ -12,8 +12,8 @@ func handleErr(err error) {
 }
 
 func main() {
-	udpsendcmd := exec.Command("udp-sender", "--async", "--file", "/samplefiles/warpeace.txt")
-	//udpsendcmd := exec.Command("udp-sender", "--async", "--fec", "8x8", "--pipe", "\"gzip -fN\"", "--file", "/samplefiles/warpeace.txt")
+	//udpsendcmd := exec.Command("udp-sender", "--async", "--file", "samplefiles/warpeace.txt")
+	udpsendcmd := exec.Command("udp-sender", "--async", "--fec", "8x8", "--pipe", "\"gzip -fN\"", "--file", "samplefiles/warpeace.txt")
 
 	sendout, err := udpsendcmd.Output()
 	handleErr(err)
