e33f3fcfa4c024e68dad51b8b68a2386b6bc301b
alanpadillachua <axp141330@utdallas.edu>
2018-06-15 07:24:45 -0700

    added main with http service listening for file request

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 GoUDPCast is a golang wrapper for UDPCast.
 
+## Usage 
+
+`curl -F "file=@[filename.type]" localhost:8080/GoUPDCast/v1/upload/`
--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	const prefix = "/GoUPDCast/v1"
+	http.HandleFunc(prefix+"/upload/", uploadHandler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("method:", r.Method)
+	if r.Method != http.MethodPost {
+		w.Header().Add("accept", http.MethodPost)
+		http.Error(w, "method must be POST", http.StatusMethodNotAllowed)
+		return
+	}
+	if err := r.ParseMultipartForm(64); err != nil {
+		log.Println(err.Error())
+	}
+
+	file, handler, fileErr := r.FormFile("file")
+
+	if fileErr != nil {
+		http.Error(w, fileErr.Error(), http.StatusInternalServerError)
+		log.Println(fileErr)
+		return
+	}
+	log.Println(handler.Filename)
+	log.Println(handler.Header)
+
+	file.Close()
 }
