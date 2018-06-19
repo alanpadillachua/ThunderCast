8a768fb630f0462b4fd0f4284d2f335789450ac2
alanpadillachua <axp141330@utdallas.edu>
2018-06-19 11:11:33 -0500

    restarting

--- main.go
+++ main.go
@@ -1,124 +1,40 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
-
-	jwtmiddleware "github.com/auth0/go-jwt-middleware"
-	jwt "github.com/dgrijalva/jwt-go"
-	"github.com/gin-gonic/contrib/static"
-	"github.com/gin-gonic/gin"
 )
 
-var jwtMiddleWare *jwtmiddleware.JWTMiddleware
-
-type Jwks struct {
-	Keys []JSONWebKeys `json:"keys"`
-}
-
-type JSONWebKeys struct {
-	Kty string   `json:"kty"`
-	Kid string   `json:"kid"`
-	Use string   `json:"use"`
-	N   string   `json:"n"`
-	E   string   `json:"e"`
-	X5c []string `json:"x5c"`
-}
-
 func main() {
-	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
-		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-			aud := os.Getenv("AUTH0_API_AUDIENCE")
-			checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
-			if !checkAudience {
-				return token, errors.New("Invalid audience.")
-			}
-			// verify iss claim
-			iss := os.Getenv("AUTH0_DOMAIN")
-			checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
-			if !checkIss {
-				return token, errors.New("Invalid issuer.")
-			}
-
-			cert, err := getPemCert(token)
-			if err != nil {
-				log.Fatalf("could not get cert: %+v", err)
-			}
-
-			result, _ := jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
-			return result, nil
-		},
-		SigningMethod: jwt.SigningMethodRS256,
-	})
-	// register our actual jwtMiddleware
-	jwtMiddleWare = jwtMiddleware
-	router := gin.Default()
-
-	router.Use(static.Serve("/", static.LocalFile("./views", true)))
-
-	api := router.Group("/api")
-	{
-		api.GET("/", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "pong",
-			})
-		})
-	}
-	api.POST("/send", authMiddleware(), sendHandler)
-
-	router.Run(":3000")
-}
-func sendHandler(c *gin.Context) {
-	c.Header("Content-Type", "application/json")
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Send File handler not implemented yet",
-	})
-}
-
-func authMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// Get the client secret key
-		err := jwtMiddleWare.CheckJWT(c.Writer, c.Request)
-		if err != nil {
-			// Token not found
-			fmt.Println(err)
-			c.Abort()
-			c.Writer.WriteHeader(http.StatusUnauthorized)
-			c.Writer.Write([]byte("Unauthorized"))
-			return
-		}
+	const prefix = "/"
+	http.HandleFunc(prefix+"/upload/", uploadHandler)
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
-func getPemCert(token *jwt.Token) (string, error) {
-	cert := ""
-	resp, err := http.Get(os.Getenv("AUTH0_DOMAIN") + ".well-known/jwks.json")
-	if err != nil {
-		return cert, err
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+
+	log.Println("method:", r.Method)
+
+	if r.Method != http.MethodPost {
+		w.Header().Add("accept", http.MethodPost)
+		http.Error(w, "method must be POST", http.StatusMethodNotAllowed)
+		return
 	}
-	defer resp.Body.Close()
-
-	var jwks = Jwks{}
-	err = json.NewDecoder(resp.Body).Decode(&jwks)
-
-	if err != nil {
-		return cert, err
+	if err := r.ParseMultipartForm(64); err != nil {
+		log.Println(err.Error())
 	}
 
-	x5c := jwks.Keys[0].X5c
-	for k, v := range x5c {
-		if token.Header["kid"] == jwks.Keys[k].Kid {
-			cert = "-----BEGIN CERTIFICATE-----\n" + v + "\n-----END CERTIFICATE-----"
-		}
-	}
+	file, handler, fileErr := r.FormFile("file")
 
-	if cert == "" {
-		return cert, errors.New("unable to find appropriate key.")
+	if fileErr != nil {
+		http.Error(w, fileErr.Error(), http.StatusInternalServerError)
+		log.Println(fileErr)
+		return
 	}
+	log.Println(handler.Filename)
+	log.Println(handler.Header)
 
-	return cert, nil
+	file.Close()
 }
