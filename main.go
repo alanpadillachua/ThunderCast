dc6ea42f7c214b7afc230c34fa33a48e70f94b27
alanpadillachua <axp141330@utdallas.edu>
2018-06-19 13:58:42 -0500

    adde basic front end for uploading files

--- main.go
+++ main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"crypto/md5"
+	"fmt"
+	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
-	const prefix = "/"
-	http.HandleFunc(prefix+"/upload/", uploadHandler)
+	const prefix = ""
+	http.HandleFunc(prefix+"/upload", upload)
+	//http.HandleFunc(prefix+"/login", login)
 	if err := http.ListenAndServe(":3000", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+/*
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //get request method
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.html")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		// logic part of log in
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
 	}
 }
 
@@ -37,4 +59,34 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println(handler.Header)
 
 	file.Close()
+}
+*/
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("upload.html")
+		t.Execute(w, token)
+	} else {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile("/test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
 }
