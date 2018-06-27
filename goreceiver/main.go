eb406cc6dc156844038ce88df0dd98990b04d897
alanpadillachua <axp141330@utdallas.edu>
2018-06-27 13:22:30 -0500

    its working

--- goreceiver/main.go
+++ goreceiver/main.go
@@ -10,22 +10,20 @@ import (
 )
 
 func main() {
-
 	router := mux.NewRouter()
+
 	router.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./Public"))))
-	//router.Handle("/", http.FileServer(http.Dir("./Public")))
-	router.Handle("/files/{fn}", http.StripPrefix("/files", http.FileServer(http.Dir("./files"))))
+	router.PathPrefix("/files/").Handler(http.StripPrefix("/files/", http.FileServer(http.Dir("files/"))))
 
 	router.HandleFunc("/listen/{fn}", listen).Methods("GET")
-	//router.HandleFunc("/listfiles", listfiles).Methods("GET")
+	router.HandleFunc("/files", files).Methods("GET")
+
 	log.Println("Receiver Server")
 	log.Println("Listening on Port 3001")
 	log.Fatal(http.ListenAndServe(":3001", router))
-	// if err := http.ListenAndServe(":3001", nil); err != nil {
-	// 	log.Fatal("ListenAndServe: ", err)
-	// }
 
 }
+
 func listen(w http.ResponseWriter, r *http.Request) {
 	log.Println("Connection from: " + r.Host)
 	params := mux.Vars(r)
@@ -33,6 +31,3 @@ func listen(w http.ResponseWriter, r *http.Request) {
 	log.Println("Listening for file ... " + filename)
 	gocastlisten.Receive(filename)
 }
-
-// func listfiles(w http.ResponseWriter, r *http.Request) {
-// }
