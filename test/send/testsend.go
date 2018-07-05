30dcb1fc5a9e0e11052cb5d5172c347d473a9539
alanpadillachua <axp141330@utdallas.edu>
2018-07-05 12:56:32 -0500

    working in test dir

--- test/send/testsend.go
+++ test/send/testsend.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/alanpadillachua/GoCast/gosender/gocastsend"
 )
 
+const receiverListenIP = "http://172.24.0.194:3001/listen/"
+const filename = "warpeace.txt"
+
 func main() {
 	log.Println("Sending file...")
-	gocastsend.Send("../../gosender/samplefiles/" + "ubuntu-18.04-desktop-amd64.iso")
+	log.Println("Making call request to listen @:" + receiverListenIP)
+	resp, err := http.Get(receiverListenIP + filename)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	resp.Body.Close()
+	gocastsend.Send("../../gosender/samplefiles/" + filename)
 }
