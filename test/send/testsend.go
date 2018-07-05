2cfc28fef662dcea42260e12b48f5e763a867be1
alanpadillachua <axp141330@utdallas.edu>
2018-07-05 14:36:53 -0500

    sync group for testing

--- test/send/testsend.go
+++ test/send/testsend.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/alanpadillachua/GoCast/gosender/gocastsend"
 )
@@ -11,9 +12,36 @@ const receiverListenIP = "http://172.24.0.194:3001/listen/"
 const filename = "warpeace.txt"
 
 func main() {
-	log.Println("Sending file...")
+	// log.Println("Sending file...")
+	// log.Println("Making call request to listen @:" + receiverListenIP)
+	// go http.Get(receiverListenIP + filename)
+	// go gocastsend.Send("../../gosender/samplefiles/" + filename)
+	// log.Panicln("Files sent")
+	//readyToSend := make(chan bool, 1)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		http.Get(receiverListenIP + filename)
+		//readyToSend <- true
+	}()
+	wg.Wait()
+	log.Println("Transfering file: " + filename)
+	gocastsend.Send("./files/" + filename) // send file through diod
+
+}
+
+func startListening(file string) {
 	log.Println("Making call request to listen @:" + receiverListenIP)
-	http.Get(receiverListenIP + filename)
-	gocastsend.Send("../../gosender/samplefiles/" + filename)
-	log.Panicln("Files sent")
+	resp, err := http.Get(receiverListenIP + file)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	resp.Body.Close()
+	log.Println("Transfering file: " + file)
+	gocastsend.Send("./files/" + file) // send file through diod
+
 }
