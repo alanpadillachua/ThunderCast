3f72505ad9955a727be05f8f5563017cf531de78
alanpadillachua <axp141330@utdallas.edu>
2018-07-05 09:05:29 -0500

    added better output for gosend

--- gosender/gocastsend/gosend.go
+++ gosender/gocastsend/gosend.go
@@ -2,8 +2,10 @@ package gocastsend
 
 import (
 	"bufio"
+	"bytes"
 	"compress/gzip"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -25,12 +27,44 @@ func Send(file string) {
 	//"--pipe", "\"gzip -f\"",
 	udpcastargs := []string{"--async", "--max-bitrate", "40m", "--fec", "8x8", "--file", file}
 	cmd := exec.Command(udpcastcmd, udpcastargs...)
-	out, err := cmd.CombinedOutput()
+	var stdoutBuf, stderrBuf bytes.Buffer
+	stdoutIn, err := cmd.StdoutPipe()
+	stderrIn, err := cmd.StderrPipe()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error creating StdoutPipe for Cmd", err)
+		os.Exit(1)
+	}
+	var errStdout, errStderr error
+	stdout := io.MultiWriter(os.Stdout, &stdoutBuf)
+	stderr := io.MultiWriter(os.Stderr, &stderrBuf)
+
+	err = cmd.Start()
+
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error starting Cmd", err)
 		os.Exit(1)
 	}
-	fmt.Printf("%s\n", out)
+	go func() {
+		_, errStdout = io.Copy(stdout, stdoutIn)
+	}()
+
+	go func() {
+		_, errStderr = io.Copy(stderr, stderrIn)
+	}()
+
+	err = cmd.Wait()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error waiting for Cmd", err)
+		os.Exit(1)
+	}
+	if errStdout != nil || errStderr != nil {
+		log.Fatal("failed to capture stdout or stderr\n")
+	}
+	outStr, outErr := string(stdoutBuf.Bytes()), string(stderrBuf.Bytes())
+
+	fmt.Println(outStr)
+	fmt.Println(outErr)
+
 }
 
 func compress(file string) {
