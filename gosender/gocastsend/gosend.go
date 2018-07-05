ac3591ee640ace5361376c49f24a28028a1f70a4
alanpadillachua <axp141330@utdallas.edu>
2018-07-05 14:02:12 -0500

    ehhh

--- gosender/gocastsend/gosend.go
+++ gosender/gocastsend/gosend.go
@@ -25,7 +25,7 @@ func Send(file string) {
 
 	udpcastcmd := "udp-sender"
 	//"--pipe", "\"gzip -f\"", "--min-receivers", "1",
-	udpcastargs := []string{"--nokbd", "--async", "--max-bitrate", "40m", "--fec", "8x8", "--file", file}
+	udpcastargs := []string{"--async", "--max-bitrate", "40m", "--fec", "8x8", "--file", file, "--nokbd"}
 	cmd := exec.Command(udpcastcmd, udpcastargs...)
 	// out, err := cmd.CombinedOutput()
 	// if err != nil {
