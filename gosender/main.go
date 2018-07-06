f9deaae12f62c439dc357c6fa1b258e9fba5ed0c
alanpadillachua <axp141330@utdallas.edu>
2018-07-06 10:37:02 -0500

    added file hash

--- gosender/main.go
+++ gosender/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"log"
@@ -75,6 +77,13 @@ func UploadFile(w http.ResponseWriter, r *http.Request) {
 	}
 	log.Println("Reading file: " + filename)
 	jsonResponse(w, http.StatusCreated, "File uploaded successfully!.")
+	filehash, err := hashFileMd5(filename)
+	if err != nil {
+		fmt.Fprintf(w, "%v", err)
+		log.Println(err.Error())
+	}
+
+	log.Println("File hash: " + filehash)
 
 	go startListening(filename)
 	time.Sleep(2 * time.Second)
@@ -91,4 +100,33 @@ func jsonResponse(w http.ResponseWriter, code int, message string) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	fmt.Fprint(w, message)
+}
+func hashFileMd5(filename string) (string, error) {
+	//Initialize variable returnMD5String now in case an error has to be returned
+	var returnMD5String string
+
+	//Open the passed argument and check for any error
+	file, err := os.Open("./files/" + filename)
+	if err != nil {
+		return returnMD5String, err
+	}
+
+	//Tell the program to call the following function when the current function returns
+	defer file.Close()
+
+	//Open a new hash interface to write to
+	hash := md5.New()
+
+	//Copy the file in the hash interface and check for any error
+	if _, err := io.Copy(hash, file); err != nil {
+		return returnMD5String, err
+	}
+
+	//Get the 16 bytes hash
+	hashInBytes := hash.Sum(nil)[:16]
+
+	//Convert the bytes to a string
+	returnMD5String = hex.EncodeToString(hashInBytes)
+
+	return returnMD5String, nil
 }
