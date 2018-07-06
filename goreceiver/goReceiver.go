91a2d77eef97bac78755544e49950d9315fcbb4f
alanpadillachua <axp141330@utdallas.edu>
2018-07-06 14:35:15 -0500

    added hash check

--- goreceiver/goReceiver.go
+++ goreceiver/goReceiver.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -17,8 +20,7 @@ func main() {
 	router.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./Public"))))
 	router.PathPrefix("/files/").Handler(http.StripPrefix("/files/", http.FileServer(http.Dir("files/"))))
 
-	router.HandleFunc("/listen/{fn}", listen).Methods("GET")
-
+	router.HandleFunc("/listen/{vars}", listen).Methods("GET").Queries("filename", "{filename}", "hash", "{hash}")
 	log.Println("Receiver Server")
 	log.Println("Listening on Port 3001")
 
@@ -30,9 +32,49 @@ func main() {
 func listen(w http.ResponseWriter, r *http.Request) {
 	log.Println("Connection from: " + r.Host)
 	params := mux.Vars(r)
-	filename := params["fn"]
-
-	//hashsum := params["hs"]
-	log.Println("Listening for file ... " + filename)
+	filename := params["filename"]
+	hashsum := params["hash"]
+	log.Println("Listening for file:" + filename)
+	log.Println("Hash of file: " + hashsum)
 	gocastlisten.Receive("./files/" + filename)
+
+	hashbuilt, err := hashFileMd5("./files/" + filename)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	if hashbuilt == hashsum {
+		log.Println("File hash verified\nFile transfered successfully")
+	} else {
+		log.Println("Error File hash integreity lost. Please retry transfer ")
+	}
+}
+
+func hashFileMd5(filename string) (string, error) {
+	//Initialize variable returnMD5String now in case an error has to be returned
+	var returnMD5String string
+
+	//Open the passed argument and check for any error
+	file, err := os.Open("./files/" + filename)
+	if err != nil {
+		return returnMD5String, err
+	}
+
+	//Tell the program to call the following function when the current function returns
+	defer file.Close()
+
+	//Open a new hash interface to write to
+	hash := md5.New()
+
+	//Copy the file in the hash interface and check for any error
+	if _, err := io.Copy(hash, file); err != nil {
+		return returnMD5String, err
+	}
+
+	//Get the 16 bytes hash
+	hashInBytes := hash.Sum(nil)[:16]
+
+	//Convert the bytes to a string
+	returnMD5String = hex.EncodeToString(hashInBytes)
+
+	return returnMD5String, nil
 }
