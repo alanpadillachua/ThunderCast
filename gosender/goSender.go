2fdf1cd3974a9d5c96bfa9ab6b1278f1d3e8c697
alanpadillachua <axp141330@utdallas.edu>
2018-07-09 09:24:44 -0500

    testing a file progress bar

--- gosender/goSender.go
+++ gosender/goSender.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/alanpadillachua/GoCast/gosender/gocastsend"
+	"github.com/gosuri/uiprogress"
 )
 
 const receiverListenIP = "http://172.24.0.194:3001/listen/v1?"
@@ -40,10 +41,13 @@ func UploadFile(w http.ResponseWriter, r *http.Request) {
 		log.Println(err.Error())
 		return
 	}
+	uiprogress.Start()            // start rendering
+	bar := uiprogress.AddBar(100) // Add a new bar
 	//copy each part to destination.
 	filename := ""
 	for {
 		part, err := reader.NextPart()
+		bar.Incr()
 		if err == io.EOF {
 			break
 		}
